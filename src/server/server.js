6962d3ce71ea6fb697b77505df17c9f4ca4c7a8a
Mac <1270201010@qq.com>
2018-05-17 17:27:15 +0800

    修改server方法

--- src/server/server.js
+++ src/server/server.js
@@ -10,16 +10,21 @@ import Vue from 'vue'
  export default{
      //get请求
      get:(url,data,load)=>{
+     	if(!load || load == undefined){
+            Vue.$indicator.open({
+                text: '加载中...',
+                spinnerType: 'fading-circle'
+              });
+        }
         return new Promise((resolve,reject)=>{
             Axios.get(url,{
                 params: data
             }).then((res)=>{
-                if(!load || load == undefined){
-                    Vue.$indicator.open({
-                        text: '加载中...',
-                        spinnerType: 'fading-circle'
-                      });
-                }
+	            
+	             if(load || load == undefined) {
+	                Vue.$indicator.close();
+                  }
+                  
                 if(res.data.code == 0){
                     resolve(res.data)
                 }else if(res.data.code == 500){
@@ -49,6 +54,7 @@ import Vue from 'vue'
                 if (load || load == undefined) {
                     Vue.$indicator.close();
                   }
+
                 Vue.$toast({
                     message: '网络好像出错了',
                     duration: 2000
@@ -68,12 +74,14 @@ import Vue from 'vue'
                 spinnerType: 'fading-circle'
               });
         }
+
         return new Promise((resolve,reject)=>{
             Axios.post(url,data)
             .then((res)=>{
                 if (load || load == undefined) {
                     Vue.$indicator.close();
                   }
+
                 if(res.data.code == 0){
                     resolve(res.data)
                 }else if(res.data.code == 500){
@@ -104,6 +112,7 @@ import Vue from 'vue'
                 if (load || load == undefined) {
                     Vue.$indicator.close();
                   }
+                  
                 Vue.$toast({
                     message: '网络好像出错了',
                     duration: 2000
